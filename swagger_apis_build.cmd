d1e2c0269cb9ae05a48b50d8ed5c7eea26596163
vvujjini <vijay.vujjini@gmail.com>
2023-02-10 13:02:52 +0530

    mapper resolve
    adding resolve end points for id and payment address lookup. refactoring for better readability.

--- swagger_apis_build.cmd
+++ swagger_apis_build.cmd
@@ -2,5 +2,5 @@
 # For each new category, please make sure to add a reference link in this file for easy auto generation of yamls.
 
 swagger-cli -f 2 -t yaml bundle api/endpoints/authz_index.yaml -o api/g2p-authz.yaml
-swagger-cli -f 2 -t yaml bundle api/endpoints/account-mapper_index.yaml -o api/g2p-account-mapper.yaml
+swagger-cli -f 2 -t yaml bundle api/endpoints/mapper_index.yaml -o api/g2p-mapper.yaml
 swagger-cli -f 2 -t yaml bundle api/endpoints/payment_index.yaml -o api/g2p-payments.yaml
