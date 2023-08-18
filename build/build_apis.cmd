7a04e8c01910af7cb1256d5734221d5d25db6f74
vvujjini <vijay.vujjini@gmail.com>
2023-08-18 11:05:35 +0530

    rename auth to authz

--- build/build_apis.cmd
+++ build/build_apis.cmd
@@ -2,12 +2,12 @@
 # Assumes the command is run from the root folder i.e ./specs
 # For each new category, please make sure to add a reference link in this file for easy auto generation of yamls.
 
-swagger-cli -f 2 -t yaml bundle ./src/authz/auth_core_api_v1.0.0.yaml -o ./release/yaml/auth_core_api_v1.0.0.yaml
+swagger-cli -f 2 -t yaml bundle ./src/authz/authz_core_api_v1.0.0.yaml -o ./release/yaml/authz_core_api_v1.0.0.yaml
 swagger-cli -f 2 -t yaml bundle ./src/registry/registry_core_api_v1.0.0.yaml -o ./release/yaml/registry_core_api_v1.0.0.yaml
 swagger-cli -f 2 -t yaml bundle ./src/mapper/mapper_core_api_v1.0.0.yaml -o ./release/yaml/mapper_core_api_v1.0.0.yaml
 # swagger-cli -f 2 -t yaml bundle ./src/mapper/mapper_core_api_v1.1.0.yaml -o ./release/yaml/mapper_core_api_v1.1.0.yaml
 
-redocly build-docs ./release/yaml/auth_core_api_v1.0.0.yaml  -o ./release/html/auth_core_api_v1.0.0.html
+redocly build-docs ./release/yaml/authz_core_api_v1.0.0.yaml  -o ./release/html/authz_core_api_v1.0.0.html
 redocly build-docs ./release/yaml/registry_core_api_v1.0.0.yaml  -o ./release/html/registry_core_api_v1.0.0.html
 redocly build-docs ./release/yaml/mapper_core_api_v1.0.0.yaml  -o ./release/html/mapper_core_api_v1.0.0.html
 # redocly build-docs ./release/yaml/mapper_core_api_v1.1.0.yaml  -o ./release/html/mapper_core_api_v1.1.0.html
